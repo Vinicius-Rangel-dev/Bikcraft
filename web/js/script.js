b014b1ba4076d5f24a77422250a58aa409cdee41
Vinicius Rangel <vinicius.rangel@live.com>
2021-06-08 18:58:25 -0300

    continuação implementação das animações

--- web/js/script.js
+++ web/js/script.js
@@ -1,4 +1,13 @@
-new SimpleSlide ({
-    slide: "quote", //nome do atributo data-slide="principal"
-    time: 5000 // tempo de transiçãp dos slides
+if (window.SimpleSlide) {
+
+	new SimpleSlide({
+  slide: "quote", // nome do atributo data-slide="principal"
+  time: 5000, // tempo de transição dos slides
 });
+
+new SimpleSlide({
+  slide: "portfolio",
+  nav: true, // nome do atributo data-slide="principal"
+  time: 5000, // tempo de transição dos slides
+});
+}
